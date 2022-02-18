ceb16e3764551d2d5022a8ab02fc8c92b71e0adf
Rico Zhu <ricozhuthegreat@hotmail.com>
2022-02-17 21:46:01 -0500

    Added Orthogonal Decomp Equation

--- utils.py
+++ utils.py
@@ -14,3 +14,18 @@ def get_rref(A, pivots=False):
 def make_augment(A, b):
     return A.row_join(b)
 
+# Algorithms useful for quick reference
+def get_x_hat(A, b):
+
+    # Returns x_hat such that bV (orthogonal decomp of b onto V) = A * x_hat
+    # Formula: x_hat = A_t * A * x_hat = A_t * b
+
+    AT = get_transpose(A)
+
+    ATA = AT * A
+    ATb = AT * b
+
+    x = make_augment(ATA, ATb)
+
+    # Solve for x_hat from new system of equations
+    return get_rref(x)
